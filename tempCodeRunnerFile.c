a9d7212efb53e6e87aee069435ac9da13259395a
adityasharma004 <adityasharma3520@gmail.com>
2022-11-15 23:21:50 +0530

    c

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+  float radius,area;
+  printf("enter the radius of a circle\n");
+  scanf("%f",&radius);
+  area = 3.14*radius*radius;
+  printf("area of circle = %2f\n",area);
+
+}
