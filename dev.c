1fc29dcdaaa0f2a40339b46a110535ad47668cc1
adityasharma004 <adityasharma3520@gmail.com>
2022-11-18 17:43:03 +0100

    c

--- dev.c
+++ dev.c
@@ -1,31 +1,41 @@
+// #include<stdio.h>
+// void main(){
+//     int week;
+//     scanf("%d",&week);
+//     switch (week)
+//     {
+//     case 1:
+//        printf("monday");
+//         break;
+//     case 2:
+//        printf("tuesday");
+//         break;
+//     case 3:
+//        printf("wednesday");
+//         break;
+//     case 4:
+//        printf("thursday");
+//         break;
+//     case 5:
+//        printf("friday");
+//         break;
+//     case 6:
+//        printf("saturday");
+//         break;
+//     case 7:
+//        printf("sunday");
+//         break;
+//     default:
+//     printf("invalid input!");
+//     }
+// }
+
+
 #include<stdio.h>
 void main(){
-    int week;
-    scanf("%d",&week);
-    switch (week)
-    {
-    case 1:
-       printf("monday");
-        break;
-    case 2:
-       printf("tuesday");
-        break;
-    case 3:
-       printf("wednesday");
-        break;
-    case 4:
-       printf("thursday");
-        break;
-    case 5:
-       printf("friday");
-        break;
-    case 6:
-       printf("saturday");
-        break;
-    case 7:
-       printf("sunday");
-        break;
-    default:
-    printf("invalid input!");
-    }
+float r , a;
+printf("enter the value of r");
+scanf("%f",&r);
+a=3.14*r*r;
+printf("area is %.2f",a);
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,41 @@
-#include<stdio.h>
-#include<math.h>
-void main(){
-  float radius,area;
-  printf("enter the radius of a circle\n");
-  scanf("%f",&radius);
-  area = 3.14*radius*radius;
-  printf("area of circle = %2f\n",area);
+// #include<stdio.h>
+// void main(){
+//     int week;
+//     scanf("%d",&week);
+//     switch (week)
+//     {
+//     case 1:
+//        printf("monday");
+//         break;
+//     case 2:
+//        printf("tuesday");
+//         break;
+//     case 3:
+//        printf("wednesday");
+//         break;
+//     case 4:
+//        printf("thursday");
+//         break;
+//     case 5:
+//        printf("friday");
+//         break;
+//     case 6:
+//        printf("saturday");
+//         break;
+//     case 7:
+//        printf("sunday");
+//         break;
+//     default:
+//     printf("invalid input!");
+//     }
+// }
 
+
+#include<stdio.h>
+void main(){
+float r , a;
+printf("enter the value of r");
+scanf("%f",&r);
+a=3.14*r*r;
+printf("area is %.2f",a);
 }
