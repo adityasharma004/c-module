c50571c55bb345fc2ab8fa357c6d530b4c29a7a1
adityasharma004 <adityasharma3520@gmail.com>
2022-11-19 22:47:25 +0530

    c

---
+++ lcm.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main (){
+    int n1,n2,max;
+    scanf("%d %d", &n1,&n2);
+      max = (n1 > n2) ? n1 : n2;
+
+    while (1) {
+        if ((max % n1 == 0) && (max % n2 == 0)) {
+            printf("The LCM of %d and %d is %d.", n1, n2, max);
+            break;
+        }
+        ++max;
+    }
+}
